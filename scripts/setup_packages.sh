7e2a31477e8622dbf09e54474692ff9b4833f565
Tobias S <github@myhome.zone>
2021-08-31 10:44:11 +0200

    Don't Install recommend packages

--- scripts/setup_packages.sh
+++ scripts/setup_packages.sh
@@ -5,7 +5,7 @@ then
   apt-get -qq update
   packages=$(cat /opt/scripts/.packages)
   for i in $packages; do
-    sudo apt-get -qq -y install $i
+    sudo apt-get install -qq -y --no-install-recommends $i
   done
 elif [ $1 == "-update" ]
 then
